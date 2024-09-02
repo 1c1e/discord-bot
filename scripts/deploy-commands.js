bebf23e61db554b6d4c80286cd408944631f8143
LCLE <108184785+1c1e@users.noreply.github.com>
2024-09-02 13:58:37 +0100

    Updated commandsPath DIR

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -9,7 +9,7 @@ const {
 const fs = require('node:fs');
 const path = require('node:path');
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
