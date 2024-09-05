62c117e20040e5234137da0994b3dfc57396b624
LCLE <108184785+1c1e@users.noreply.github.com>
2024-09-05 14:14:10 +0100

    Moved to scripts directory

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: "Discord.JS Bot",
-    script: "pnpm",
-    args: "run start",
-    time: true,
-    exp_backoff_restart_delay : "100"
-  }]
-}
---
+++ scripts/ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	apps: [{
+		name: 'Discord.JS Bot',
+		script: 'pnpm',
+		args: 'run start',
+		time: true,
+		exp_backoff_restart_delay: '100',
+	}],
+};
