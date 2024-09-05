a8e2b26bd9d5177a00c9e77cc29976dbfd7b1a4a
LCLE <108184785+1c1e@users.noreply.github.com>
2024-09-05 14:14:40 +0100

    Added prom-client module

---
+++ scripts/promClient.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+	collectDefaultMetrics,
+	register,
+} = require('prom-client');
+collectDefaultMetrics();
+const startMetricsServer = () => {
+	const app = express();
+	app.get('/metrics', async (_req, res) => {
+		try {
+			res.set('Content-Type', register.contentType);
+			res.end(await register.metrics());
+		}
+		catch (err) {
+			res.status(500).end(err);
+		}
+	});
+	app.listen(4001, '0.0.0.0');
+};
+module.exports = {
+	startMetricsServer,
+};
